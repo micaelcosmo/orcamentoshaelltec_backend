d20188552d0609f6907a7209f43be22687af3a6e
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-14 21:37:26 -0300

    Update .gitignore, app.py, and 3 more files... Adicionado passo a passo comentado a ser implementado. Criado integração com Email no app.py Adicionado template html gratuíto para servir de base para o envio de Email.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from chalice import Chalice
 
+from chalicelib.email import EmailSender
+
 app = Chalice(app_name='orcamentoshaelltec')
 
 @app.route('/orcamento', methods=['POST'])
@@ -7,6 +9,28 @@ def solicitar_orcamento():
 #   # This is the JSON body the user sent in their POST request.
     orcamento = app.current_request.json_body
 #   # Disparar email para Haelltec
+    ## montar email
+    ## enviar email montado
 #   # Se disparou com sucesso, guarda no banco
 #   # Se não, loga o erro
     return {}
+
+def salvar_orcamento(orcamento):
+    try:
+        print(orcamento)
+# guarda orcamento com status solicitada
+    except Exception as e:
+        print(e, "Erro ao salvar orçamento")
+
+
+def montar_email():
+# ler o template.html e carregar na variavel
+# substituir as variaveis (replace)
+# retornar string email com os valores substituidos
+    return
+
+def guarda_banco():
+    return
+
+def enviar_email(orcamento):
+    return EmailSender.enviar_email(orcamento)
