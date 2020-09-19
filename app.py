38e4497b6c36433ac136918617f4627762890825
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-19 06:13:14 -0300

    Update app.py Corrigido sintaxe no operador de formatação.

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def solicitar_orcamento():
 
 
 def calcular_valor_total(orcamento):
-    orcamento['total'] = "%.2f" % calcular(orcamento)
+    orcamento['total'] = "{:.2f}" % calcular(orcamento)
     return orcamento
 
 
