7de535c2021ad706213026df84b2a0bb2e10f426
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-19 02:48:24 -0300

    Update app.py Atualizando chamada da classe que estava com erro de sintaxe

--- app.py
+++ app.py
@@ -4,29 +4,32 @@ from chalicelib.email import EmailSender
 
 app = Chalice(app_name='orcamentoshaelltec')
 
+
 @app.route('/orcamento', methods=['POST'])
 def solicitar_orcamento():
-#   # This is the JSON body the user sent in their POST request.
+    #   # This is the JSON body the user sent in their POST request.
+    print(app.current_request.json_body)
     orcamento = app.current_request.json_body
     try:
-        enviar_email(orcamento)
-        #   # Se disparou com sucesso, guarda no banco
+        response = enviar(orcamento)
+        salvar(orcamento)
     except Exception as e:
         print('Erro desconhecido, solicitanto orçamento.')
         print(e)
     return {}
 
-def salvar_orcamento(orcamento):
+
+def salvar(orcamento):
     try:
         print(orcamento)
-# guarda orcamento com status solicitada
+    # guarda orcamento com status solicitada
     except Exception as e:
         print(e, "Erro ao salvar orçamento")
-
 
 
 def guarda_banco():
     return
 
-def enviar_email(orcamento):
-    return EmailSender.enviar_email(orcamento)
+
+def enviar(orcamento):
+    return EmailSender().enviar_email(orcamento)
